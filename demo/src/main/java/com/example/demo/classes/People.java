dab85935e3618af67c5c06789eded4e96d944d7c
NFintak <natefintak@gmail.com>
2025-08-23 13:52:42 -0400

    checkpoint

--- demo/src/main/java/com/example/demo/classes/People.java
+++ demo/src/main/java/com/example/demo/classes/People.java
@@ -1,7 +1,22 @@
 package com.example.demo.classes;
 
-public abstract class People {
+import java.util.List;
+import java.util.ArrayList;
 
+public abstract class People<PersonType extends Person> {
 
+    List<PersonType> personList;
+
+    public People(List<PersonType> personList) {
+        this.personList = personList;
+    }
+
+    public People(int number) {
+        this.personList = new ArrayList<PersonType>(number);
+    }
+
+    public void addPersonType(PersonType person) {
+        this.personList.add(person);
+    }
 
 }
