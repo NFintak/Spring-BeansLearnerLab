29e10d8e69313137f9a4b28deb4402239b9dcfd8
NFintak <natefintak@gmail.com>
2025-08-24 18:25:05 -0400

    InstructorTests in progress

--- demo/src/test/java/com/example/demo/InstructorsTests.java
+++ demo/src/test/java/com/example/demo/InstructorsTests.java
@@ -23,4 +23,50 @@ public class InstructorsTests {
         Instructors test = new Instructors(5);
         assertNotNull(test);
     }
+
+    @Test
+    public void testInstanceOfPeople() {
+        Instructors test = new Instructors(instructorList);
+        assertInstanceOf(People.class, test);
+    }
+
+    @Test
+    public void testAdd() {
+        Instructors test = new Instructors(instructorList);
+        test.add(new Instructor());
+        assertEquals(1, test.size());
+    }
+
+    @Test
+    public void testRemove() {
+        Instructors test = new Instructors(instructorList);
+        Instructor instructor1 = new Instructor(), instructor2 = new Instructor();
+        test.add(instructor1); test.add(instructor2);
+        assertEquals(2, test.size());
+        test.remove(instructor1);
+        assertEquals(1, test.size());
+    }
+
+    @Test
+    public void testClear() {
+        Instructors test = new Instructors(instructorList);
+        Instructor instructor1 = new Instructor(), instructor2 = new Instructor();
+        test.add(instructor1); test.add(instructor2);
+        assertEquals(2, test.size());
+        test.clear();
+        assertEquals(0, test.size());
+    }
+
+    @Test
+    public void testAddAll() {
+        Instructors test = new Instructors(instructorList);
+        Instructor instructor1 = new Instructor(), instructor2 = new Instructor();
+        test.add(instructor1); test.add(instructor2);
+        assertEquals(2, test.size());
+        Instructor instructor3 = new Instructor(), instructor4 = new Instructor();
+        addedList.add(instructor3); addedList.add(instructor4);
+        test.addAll(addedList);
+        assertEquals(4, test.size());
+    }
+
 }
