76be0ee73e0229c403c8e416f237e5cd8ad07931
NFintak <natefintak@gmail.com>
2025-08-24 15:56:51 -0400

    InstructorTests in progress

--- demo/src/test/java/com/example/demo/InstructorTests.java
+++ demo/src/test/java/com/example/demo/InstructorTests.java
@@ -1,4 +1,32 @@
 package com.example.demo;
 
+import com.example.demo.classes.Instructor;
+import com.example.demo.classes.Student;
+import com.example.demo.classes.Person;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class InstructorTests {
+
+    @Test
+    public void testNullConstructor() {
+        Instructor test = new Instructor();
+        assertNotNull(test);
+    }
+
+    @Test
+    public void testInstanceOf() {
+        Instructor test = new Instructor();
+        assertInstanceOf(Person.class, test);
+    }
+
+    @Test
+    public void testTeach() {
+        Instructor test = new Instructor();
+        Student student = new Student();
+        assertEquals(0.0, student.getTotalStudyTime());
+        test.teach(student, 2.5);
+        assertEquals(2.5, student.getTotalStudyTime());
+    }
+
 }
